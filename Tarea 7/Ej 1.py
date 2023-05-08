e077ba50e4735b0e49b1613beba0666dfa15f697
muertexer <69734563+muertexer@users.noreply.github.com>
2023-05-07 22:44:22 -0400

    Add files via upload

---
+++ Tarea 7/Ej 1.py
@@ -0,0 +1,147 @@
+import sys
+
+from PyQt6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PyQt6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PyQt6.QtWidgets import (QApplication, QGridLayout, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QStatusBar, QVBoxLayout,
+    QWidget)
+
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(659, 256)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayoutWidget = QWidget(self.centralwidget)
+        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
+        self.gridLayoutWidget.setGeometry(QRect(200, 20, 239, 141))
+        self.gridLayout = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton = QPushButton(self.gridLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
+
+        self.pushButton_4 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.gridLayout.addWidget(self.pushButton_4, 2, 1, 1, 1)
+
+        self.pushButton_2 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.gridLayout.addWidget(self.pushButton_2, 1, 0, 1, 1)
+
+        self.pushButton_3 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.gridLayout.addWidget(self.pushButton_3, 2, 0, 1, 1)
+
+        self.pushButton_5 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+
+        self.gridLayout.addWidget(self.pushButton_5, 2, 2, 1, 1)
+
+        self.pushButton_6 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+
+        self.gridLayout.addWidget(self.pushButton_6, 1, 1, 1, 1)
+
+        self.pushButton_7 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+
+        self.gridLayout.addWidget(self.pushButton_7, 0, 1, 1, 1)
+
+        self.pushButton_8 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+
+        self.gridLayout.addWidget(self.pushButton_8, 0, 2, 1, 1)
+
+        self.pushButton_9 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+
+        self.gridLayout.addWidget(self.pushButton_9, 1, 2, 1, 1)
+
+        self.verticalLayoutWidget_2 = QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayoutWidget_2.setGeometry(QRect(470, 20, 161, 141))
+        self.verticalLayout_2 = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label_3 = QLabel(self.verticalLayoutWidget_2)
+        self.label_3.setObjectName(u"label_3")
+
+        self.verticalLayout_2.addWidget(self.label_3)
+
+        self.pushButton_11 = QPushButton(self.verticalLayoutWidget_2)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+
+        self.verticalLayout_2.addWidget(self.pushButton_11)
+
+        self.verticalLayoutWidget_3 = QWidget(self.centralwidget)
+        self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
+        self.verticalLayoutWidget_3.setGeometry(QRect(20, 20, 161, 141))
+        self.verticalLayout_3 = QVBoxLayout(self.verticalLayoutWidget_3)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.label_5 = QLabel(self.verticalLayoutWidget_3)
+        self.label_5.setObjectName(u"label_5")
+
+        self.verticalLayout_3.addWidget(self.label_5)
+
+        self.pushButton_10 = QPushButton(self.verticalLayoutWidget_3)
+        self.pushButton_10.setObjectName(u"pushButton_10")
+
+        self.verticalLayout_3.addWidget(self.pushButton_10)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(190, 180, 261, 51))
+        font = QFont()
+        font.setPointSize(28)
+        font.setBold(True)
+        self.label.setFont(font)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_8.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_9.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"NOMBRE", None))
+        self.pushButton_11.setText(QCoreApplication.translate("MainWindow", u"Jugador 2", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"NOMBRE", None))
+        self.pushButton_10.setText(QCoreApplication.translate("MainWindow", u"Jugador 1", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"EN PROGRESO", None))
+    # retranslateUi
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ventana = Ui_MainWindow()
+    ventana.show()
+
+    app.exec()
